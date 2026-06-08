2ee887a4865bcbb7d5a2b1f97600517ef3f84cb1
Makapowka3 <64094750+Makapowka3@users.noreply.github.com>
2026-06-08 15:26:32 +0100

    Add: word-pattern - submission-10

---
+++ Data Structures & Algorithms/word-pattern/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        p_to_s = {}
+        s_to_p = {}
+
+        s = s.split(' ')
+
+        if len(s) != len(pattern):
+            return False
+
+        for word, ch in zip(s, pattern):
+            if word in s_to_p and s_to_p[word] != ch:
+                return False
+            if ch in p_to_s and p_to_s[ch] != word:
+                return False
+
+            p_to_s[ch] = word
+            s_to_p[word] = ch
+
+        return True
