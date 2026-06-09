32611657a2a6181ee04054191254f431bece65cf
Makapowka3 <64094750+Makapowka3@users.noreply.github.com>
2026-06-09 13:25:01 +0100

    Add: move-zeroes - submission-3

---
+++ Data Structures & Algorithms/move-zeroes/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        write = 0
+        for n in nums:
+            if n != 0:
+                nums[write] = n
+                write += 1
+        while write < len(nums):
+            nums[write] = 0
+            write += 1
+        
