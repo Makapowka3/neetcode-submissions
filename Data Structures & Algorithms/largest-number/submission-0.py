4de3fb4d4a7eb115623b9fc36c996df04111725f
Makapowka3 <64094750+Makapowka3@users.noreply.github.com>
2026-06-30 13:32:22 +0300

    Add: largest-number - submission-0

---
+++ Data Structures & Algorithms/largest-number/submission-0.py
@@ -0,0 +1,13 @@
+from functools import cmp_to_key
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])
+
+        def compare(n1, n2):
+            if int(n1 + n2) > int(n2 + n1):
+                return -1
+            return 1
+        
+        nums = sorted(nums, key=cmp_to_key(compare))
+        return str(int(''.join(nums)))
