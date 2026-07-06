5d6195c6ebf39a0babacc25fd5e131beba12b2af
Makapowka3 <64094750+Makapowka3@users.noreply.github.com>
2026-07-06 11:55:02 +0300

    Add: path-crossing - submission-2

---
+++ Data Structures & Algorithms/path-crossing/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        current = [0,0]
+        path_set = {(0,0)}
+
+        for p in path:
+            if p == 'N':
+                current[0] += 1
+            elif p == 'S':
+                current[0] -= 1
+            elif p == 'E':
+                current[1] -= 1
+            else:
+                current[1] += 1
+
+            path_set.add(tuple(current))
+
+        return len(path_set) != len(path) + 1
