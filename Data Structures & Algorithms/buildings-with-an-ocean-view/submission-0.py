48af904c2a2f9ddc672c68911f7f48277a027490
Makapowka3 <64094750+Makapowka3@users.noreply.github.com>
2026-09-25 16:21:17 +0300

    Add: buildings-with-an-ocean-view - submission-0

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        res = []
+
+        curr_max = 0
+        for i in range(len(heights)-1, -1, -1):
+            if heights[i] > curr_max:
+                res.append(i)
+            curr_max = max(curr_max, heights[i])
+        
+        res.reverse()
+        return res
